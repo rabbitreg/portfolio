21563a62966f725224a68e7611d6c61dbe0e868a
rabbitreg <jiayidaoshi@gmail.com>
2022-04-06 19:38:02 +0900

    Create _slug.vue

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <artcile>
+    <h1>{{ article.title }}</h1>
+    <NuxtContent :document="article" />
+  </artcile>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('blog', params.slug).fetch()
+    return {
+      article,
+    }
+  },
+}
+</script>
